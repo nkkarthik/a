1a7d329c110922073c568d928818048d7d6d1ed8
nkkarthik <nkkarthik@gmail.com>
2025-07-11 15:18:46 -0700

    m

--- xc/m1/m1/ContentView.swift
+++ xc/m1/m1/ContentView.swift
@@ -16,14 +16,19 @@ struct ContentView: View {
             LocalhostWebView(
                 url: url,
                 urlMappings: [
+                    // scratch m / k
                     "1": "http://localhost:30001/?folder=/Users/knannuru/m",
                     "2": "https://k/?folder=/home/knannuru/k",
-                    "3": "http://localhost:30001/?workspace=/Users/knannuru/m/.vscode/cisd.code-workspace",
-                    "4": "https://k/?workspace=/home/knannuru/k/.vscode/cisd.code-workspace",
+                    // learn
+                    "3": "http://localhost:30001/?workspace=/Users/knannuru/m/.vscode/l.code-workspace",
+                    "4": "https://k/?workspace=/home/knannuru/k/.vscode/l.code-workspace",
+                    // mine github
                     "5": "http://localhost:30001/?workspace=/Users/knannuru/m/.vscode/gh.code-workspace",
                     "6": "https://k/?workspace=/home/knannuru/k/.vscode/gh.code-workspace",
-                    "7": "http://localhost:30001/?workspace=/Users/knannuru/m/.vscode/l.code-workspace",
-                    "8": "https://k/?workspace=/home/knannuru/k/.vscode/l.code-workspace",
+                    // work
+                    "7": "http://localhost:30001/?workspace=/Users/knannuru/m/.vscode/cisd.code-workspace",
+                    "8": "https://k/?workspace=/home/knannuru/k/.vscode/cisd.code-workspace",
+                    // help
                     "9": "https://chatgpt.com",
                 ]
             )
