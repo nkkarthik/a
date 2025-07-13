8766b15f61665e96e3c877daa924ce49bb1ee176
nkkarthik <nkkarthik@gmail.com>
2025-07-12 17:13:29 -0700

    m

--- xc/m1/m1/LocalhostWrapperApp.swift
+++ xc/m1/m1/LocalhostWrapperApp.swift
@@ -7,19 +7,19 @@
 
 import SwiftUI
 import WebKit
-
-#if DEBUG
-let isDebug = true
-#else
-let isDebug = false
-#endif
+import M1Library
 
 
-struct LocalhostWebView: NSViewRepresentable {
+public struct LocalhostWebView: NSViewRepresentable {
     let url: String
     let urlMappings: [String: String]
     
-    func makeNSView(context: Context) -> WKWebView {
+    public init(url: String, urlMappings: [String : String]) {
+        self.url = url
+        self.urlMappings = urlMappings
+    }
+    
+    public func makeNSView(context: Context) -> WKWebView {
         let configuration = WKWebViewConfiguration()
         
         // Enable JavaScript
@@ -28,7 +28,7 @@ struct LocalhostWebView: NSViewRepresentable {
         configuration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
         configuration.preferences.setValue(true, forKey: "javaScriptCanAccessClipboard")
         configuration.preferences.setValue(true, forKey: "DOMPasteAllowed")
-                
+        
         
         // Create webview
         let webview = CustomWKWebView(frame: .zero, configuration: configuration)
@@ -39,100 +39,102 @@ struct LocalhostWebView: NSViewRepresentable {
         return webview
     }
     
-    func updateNSView(_ webView: WKWebView, context: Context) {
+    public func updateNSView(_ webView: WKWebView, context: Context) {
         // load initial
         guard let url = URL(string: self.url) else { return }
         let request = URLRequest(url: url)
         webView.load(request)
     }
     
-    func makeCoordinator() -> Coordinator {
+    public func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
-    class Coordinator: NSObject, WKNavigationDelegate, WKUIDelegate {
-        let parent: LocalhostWebView
-        
-        init(_ parent: LocalhostWebView) {
-            self.parent = parent
+    
+}
+
+public class Coordinator: NSObject, WKNavigationDelegate, WKUIDelegate {
+    let parent: LocalhostWebView
+    
+    init(_ parent: LocalhostWebView) {
+        self.parent = parent
+    }
+    deinit {
+        print("Coordinator deinitialized")
+    }
+    
+    public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("Start loading ")
+    }
+    
+    public func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        print("Failed to load: \(error.localizedDescription)")
+    }
+    
+    public   func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        // ensure webview becomes first responder after loading
+        webView.becomeFirstResponder()
+        print("Finished loading")
+    }
+    
+    // auth challenge https://k self signed cert
+    public func webView(_ webView: WKWebView,
+                        didReceive challenge: URLAuthenticationChallenge,
+                        completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void)
+    {
+        guard let trust = challenge.protectionSpace.serverTrust else {
+            return completionHandler(.performDefaultHandling, nil)
         }
-        deinit {
-            print("Coordinator deinitialized")
-        }
-        
-        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-            print("Start loading ")
-        }
-        
-        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-            print("Failed to load: \(error.localizedDescription)")
-        }
-        
-        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            // ensure webview becomes first responder after loading
-            webView.becomeFirstResponder()
-            print("Finished loading")
-        }
-        
-        // auth challenge https://k self signed cert
-        func webView(_ webView: WKWebView,
-                     didReceive challenge: URLAuthenticationChallenge,
-                     completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void)
-        {
-            guard let trust = challenge.protectionSpace.serverTrust else {
-                return completionHandler(.performDefaultHandling, nil)
-            }
-            // Trust self-signed certificate for development only
-            let exceptions = SecTrustCopyExceptions(trust)
-            SecTrustSetExceptions(trust, exceptions)
-            let credential = URLCredential(trust: trust)
-            completionHandler(.useCredential, credential)
-        }
-        
-        // navigation clicks
-        func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
-            print("can go? \(navigationAction)")
-            guard navigationAction.targetFrame == nil,
-                  let url = navigationAction.request.url else {
-                return nil
-            }
-            NSWorkspace.shared.open(url)
+        // Trust self-signed certificate for development only
+        let exceptions = SecTrustCopyExceptions(trust)
+        SecTrustSetExceptions(trust, exceptions)
+        let credential = URLCredential(trust: trust)
+        completionHandler(.useCredential, credential)
+    }
+    
+    // navigation clicks
+    public func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        print("can go? \(navigationAction)")
+        guard navigationAction.targetFrame == nil,
+              let url = navigationAction.request.url else {
             return nil
         }
-        
-        // navigation action
-        //        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping @MainActor (WKNavigationActionPolicy) -> Void) {
-        //            print("can go? \(navigationAction)")
-        //            decisionHandler(.allow)
-        //        }
-        //        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        //            func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, ) -> Void) async -> WKNavigationActionPolicy {
-        //            print("\(navigationAction)")
-        //            if navigationAction.navigationType == .linkActivated {
-        //                if let url = navigationAction.request.url {
-        //                    print("cancel: open \(url)")
-        ////                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-        //                    NSWorkspace.shared.open(url)
-        //                    decisionHandler(.cancel)
-        //                    return
-        //                }
-        //            }
-        //            print("allow")
-        //            decisionHandler(.allow)
-        //        }
-        
-        
+        NSWorkspace.shared.open(url)
+        return nil
     }
+    
+    // navigation action
+    //        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping @MainActor (WKNavigationActionPolicy) -> Void) {
+    //            print("can go? \(navigationAction)")
+    //            decisionHandler(.allow)
+    //        }
+    //        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+    //            func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, ) -> Void) async -> WKNavigationActionPolicy {
+    //            print("\(navigationAction)")
+    //            if navigationAction.navigationType == .linkActivated {
+    //                if let url = navigationAction.request.url {
+    //                    print("cancel: open \(url)")
+    ////                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    //                    NSWorkspace.shared.open(url)
+    //                    decisionHandler(.cancel)
+    //                    return
+    //                }
+    //            }
+    //            print("allow")
+    //            decisionHandler(.allow)
+    //        }
+    
+    
 }
 
 class CustomWKWebView: WKWebView {
     private var urlMappings: [String: String] = [:]
     
-    override var acceptsFirstResponder: Bool { return true }
+    public override var acceptsFirstResponder: Bool { return true }
     
-    override func becomeFirstResponder() -> Bool { return true }
+    public override func becomeFirstResponder() -> Bool { return true }
     
-    override func keyDown(with event: NSEvent) {
+    public override func keyDown(with event: NSEvent) {
         if event.modifierFlags.contains(.command) {
             if let keys = event.charactersIgnoringModifiers, keys.count == 1 {
                 if let urlString = urlMappings[keys] {
