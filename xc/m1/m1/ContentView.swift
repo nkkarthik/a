971e0e6949648592fc7b55cc528a58d8d9b9ca05
nkkarthik <nkkarthik@gmail.com>
2025-07-14 12:53:42 -0700

    a

--- xc/m1/m1/ContentView.swift
+++ xc/m1/m1/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var url = "http://localhost:30001/?folder=/Users/knannuru/m"
-     
-
+    
+    private var url3: String
+    
+    init() {
+        self.url3 = Bundle.main.object(forInfoDictionaryKey: "URL3") as! String
+    }
+    
     var body: some View {
         VStack {
             LocalhostWebView(
@@ -20,8 +25,8 @@ struct ContentView: View {
                     "1": "http://localhost:30001/?workspace=/Users/knannuru/m/.vscode/m.code-workspace",
                     // k (quite light / one dark pro)
                     "2": "https://k/?workspace=/home/knannuru/k/.vscode/k.code-workspace",
-                    // work
-                    "3": "https://k/?workspace=/home/knannuru/k/.vscode/cisd.code-workspace",
+                    // work (light modern / dark modern)
+                    "3": url3,
                     // help
                     "9": "https://chatgpt.com",
                 ]
