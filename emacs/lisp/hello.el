ac0ad8a0546e8f8e27c91d95c390ee473611871a
nkkarthik <nkkarthik@gmail.com>
2025-07-26 00:10:16 +0000

    a

--- emacs/lisp/hello.el
+++ emacs/lisp/hello.el
@@ -14,3 +14,22 @@
     (insert body)
     (display-buffer (current-buffer))))
 
+;; get json using request
+(use-package request)
+(require 'request)
+
+(request "http://localhost:4000/api/hello"
+  :parser 'json-read
+  :encoding 'utf-8-lang
+  :success (cl-function
+	    (lambda (&key data &allow-other-keys)
+	      (let ((buf (get-buffer-create "*hello*")))
+		(with-current-buffer buf
+		  (erase-buffer)
+		  (insert (json-encode data))
+		  (json-pretty-print-buffer))
+		(display-buffer buf))))
+  :error (cl-function
+	  (lambda (&rest _)
+	    (message "failed"))))
+
--- l/pg.lisp
+++ l/pg.lisp
@@ -2,6 +2,9 @@
 (ql:quickload 'postmodern)
 
 (postmodern:connect-toplevel "knannuru" "knannuru" "" "k")
+
 (postmodern:query "select * from test")
+
+
 (postmodern:disconnect-toplevel)
 
